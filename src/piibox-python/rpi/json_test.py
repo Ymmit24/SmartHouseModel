d4a0a4dc9b7e759dc3825d9198b04aab40dd23c0
Ymmit24 <93295781+Ymmit24@users.noreply.github.com>
2022-03-17 14:21:33 +0000

    updated graph plot for actual and forecast with ref to time

--- src/piibox-python/rpi/json_test.py
+++ src/piibox-python/rpi/json_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 import json
 import urllib.request, json
@@ -13,22 +14,31 @@ print(type(raw_data))
 timestamp=[]
 actual=[]
 forecast=[]
+
 for row in raw_data:
     timestamp.append(np.datetime64(row['from']))
     intensity1 = row['intensity']
     actual.append(intensity1['actual'])
-    #print(timestamp)
 
-for row in raw_data:
-    #timestamp.append(np.datetime64(row['from']))
     intensity2 = row['intensity']
     forecast.append(intensity2['forecast'])
 
-xData = np.array(timestamp)
-yData1 = np.array(actual)
-yData2 = np.array(forecast)
-plt.plot(xData, yData1, color='b', linestyle='-', marker='o', label='y1 data')
-plt.plot(xData, yData2, color='r', linestyle='-', marker='o', label='y2 data')
+print(np.sort(forecast))
+# xData = np.array(timestamp)
+# yData1 = np.array(actual)
+# yData2 = np.array(forecast)
+
+
+fig, ax = plt.subplots(1,1)
+ax.plot(timestamp, actual, color='b', linestyle='-', marker='o', label='y1 data')
+ax.plot(timestamp, forecast, color='r', linestyle='-', marker='o', label='y2 data')
+
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m %H:%M'))
+
+for label in ax.get_xticklabels(which='major'):
+    label.set(rotation=30, horizontalalignment='right')
+
+plt.grid(True)
 plt.show()
 plt.savefig('static/img/plot2.png')
 
